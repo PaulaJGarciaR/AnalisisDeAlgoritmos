0c0b6dee2cb7451ddf4877fdb8b2992725f412f6
PaulaJGarciaR <pjgarciar@ufpso.edu.co>
2024-02-28 19:38:45 -0500

    Ejercicios repaso propuestos clase 28/02/2024

---
+++ EjercicioRepaso/src/ejerciciorepaso/EjercicioRepaso.java
@@ -0,0 +1,39 @@
+
+package ejerciciorepaso;
+
+import java.util.Scanner;
+
+public class EjercicioRepaso {
+    public static void main(String[] args) {
+//        PRIMERA OPCIÓN----------------------------------
+//       int m;
+//        System.out.println("Ingrese un numero:");
+//        Scanner xx=new Scanner(System.in);
+//        m=xx.nextInt();
+//        int r =m*m;
+//        System.out.println(r);
+
+
+//SEGUNDA OPCIÓN-------------------------------------------
+//    int m,i,n;
+//    m=0;
+//        System.out.println("Digite un numero:");
+//        Scanner xx=new Scanner(System.in);
+//        n=xx.nextInt();
+//    for(i=0;i<=n-1;i++){
+//        m=m+10;
+//         System.out.println(m);
+//    }
+     
+       //TERCERA OPCIÓN------------------------------------
+       int m=0;
+       for(int i=0;i<10;i++){
+           for(int j=0;j<10;j++){
+               m++;
+           }
+            System.out.println(m);
+       }
+
+  }
+    
+    }
